04c6adbc792f686e458eda8c36931c6dd94c18ac
eenemeenemuu <eenemeenemuu@googlemail.com>
2023-02-20 18:33:03 +0100

    Updated default values to match range for HM-300/350/400 in 24V setup.

--- hm_control_config.py
+++ hm_control_config.py
@@ -3,8 +3,8 @@ hm_control_cfg_inverter_ser = 116180215597
 hm_control_cfg_channel = 23 # [3,23,40,61,75]
 hm_control_cfg_pa_level = -18 # [-18,-12,-6,0]
 
-hm_control_cfg_inverter_power_min = 30 # power that should be generated at minimum
-hm_control_cfg_inverter_power_max = 300 # power that should be generated at maximum
+hm_control_cfg_inverter_power_min = 20 # power that should be generated at minimum
+hm_control_cfg_inverter_power_max = 200 # power that should be generated at maximum
 hm_control_cfg_inverter_power_multiplier = 1 # e.g. set to "2", if a set power limit of 50 W results in 100 W generation
 
 hm_control_cfg_power_target = 0 # set to < 0 if you want to generate more power than needed; set to > 0 to generate less power than needed
@@ -15,4 +15,4 @@ hm_control_cfg_power_set_pause = 5 # delay after power adjustment
 hm_control_cfg_shelly3em = '192.168.1.2' # host name or ip address of Shelly 3EM
 
 hm_control_cfg_fail_threshold = 30 # how often may the retrieval of the energy consumption fail before "hm_control_cfg_fail_power_limit" is applied?
-hm_control_cfg_fail_power_limit = hm_control_cfg_inverter_power_min # power that should be generated if energy consumption cannot be retrieved from Shelly 3EM
+hm_control_cfg_fail_power_limit = hm_control_cfg_inverter_power_min # power that should be generated if energy consumption cannot be retrieved more than "hm_control_cfg_fail_threshold" from Shelly 3EM; set to desired value or to "0" if you want to turn off inverter
