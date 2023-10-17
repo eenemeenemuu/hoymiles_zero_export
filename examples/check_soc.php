2cdf92ff3e17d1eec5210952050e88ec5a801cf6
eenemeenemuu <eenemeenemuu@googlemail.com>
2023-10-17 10:08:54 +0200

    Replaced if/else construct in example script.

--- examples/check_soc.php
+++ examples/check_soc.php
@@ -1,12 +1,24 @@
 <?php
 /* Example script for "temporary override min/max power values" functionality
  *
- * The quite complex if/else construct below is due to the fact that an additional battery protection
+ * The $power_max_array construct below is due to the fact that an additional hardware battery protection
  * has been installed, which should not trigger prematurely due to voltage drops (caused by high currents).
+ * This way we can smoothly ramp up and down the inverter on low SOC.
  *
- * If you don't have an additional battery protection, you can simply set the shutdown level for the desired SOC.
+ * If you don't have an additional battery protection (or don't want a soft ramp up/down)
+ * you can simply set the shutdown value for the desired SOC, like: $power_max_array = [20 => 0];
  * */
 
+$power_max_array = [
+    18 => 0,    // if SOC is below 18 % set inverter maximum power to 0 W (turn off)
+    19 => 25,   // if SOC is below 19 % set inverter maximum power to 25 W
+    20 => 50,   // and so on...
+    21 => 75,
+    22 => 100,
+    23 => 125,
+    24 => 150,
+    25 => 175
+];
 $wait = 10;
 $context = stream_context_create(['http' => ['timeout' => $wait]]);
 
@@ -21,30 +33,29 @@ while (date('Hi') == $this_Hi) { // run only in the current minute, so the cronj
         $d = explode(".", $data[0]); // date in format d.m.Y
         $t = explode(":", $data[1]); // time in format H:i:s
         if (time() < mktime($t[0], $t[1], $t[2], $d[1], $d[0], $d[2]) + 60) { // check if the data is not outdated
-            if ($data[3] < 16) {                                    // if SOC is below 16 %
-                file_put_contents_print('inverter_power_max', 0);   // set inverter maximum power to 0 W (turn off)
-            } elseif ($data[3] < 17 && $data[2] < 0) {              // if SOC is below 17 % and the battery is currently discharging
-                file_put_contents_print('inverter_power_max', 25);  // set inverter maximum power to 25 W
-            } elseif ($data[3] < 17) {                              // if SOC is below 17 %
-                file_put_contents_print('inverter_power_max', 50);  // set inverter maximum power to 50 W
-            } elseif ($data[3] < 18 && $data[2] < 0) {              // if SOC is below 18 % and the battery is currently discharging
-                file_put_contents_print('inverter_power_max', 50);  // set inverter maximum power to 50 W
-            } elseif ($data[3] < 18) {                              // if SOC is below 18 %
-                file_put_contents_print('inverter_power_max', 75);  // set inverter maximum power to 75 W
-            } elseif ($data[3] < 19 && $data[2] < 0) {              // if SOC is below 19 % and the battery is currently discharging
-                file_put_contents_print('inverter_power_max', 75);  // set inverter maximum power to 75 W
-            } elseif ($data[3] < 19) {                              // if SOC is below 19 %
-                file_put_contents_print('inverter_power_max', 100); // set inverter maximum power to 100 W
-            } elseif ($data[3] < 20 && $data[2] < 0) {              // if SOC is below 20 % and the battery is currently discharging
-                file_put_contents_print('inverter_power_max', 100); // set inverter maximum power to 100 W
-            } elseif ($data[3] < 20) {                              // if SOC is below 20 %
-                file_put_contents_print('inverter_power_max', 150); // set inverter maximum power to 150 W
-            } elseif ($data[3] < 25 && $data[2] < 0) {              // if SOC is below 25 % and the battery is currently discharging
-                file_put_contents_print('inverter_power_max', 150); // set inverter maximum power to 150 W
-            } elseif ($data[3] >= 99) {                             // if SOC is greater or equal 99 % (battery is full)
-                file_put_contents_print('inverter_power_min', 200); // set inverter minimum power to 200 W
+            $limit = [];
+            if ($data[3] >= 99) {   // if SOC is greater or equal 99 % (battery is full)
+                $limit['min'] = 150;    // set inverter minimum power to 150 W
             } else {
+                foreach ($power_max_array as $soc => $max) {
+                    if ($data[3] < $soc) {
+                        $limit['max'] = $max;
+                        if ($limit['max'] > 0 && $data[2] > 0) {    // battery is currently not discharging
+                            $limit['max'] += 25;                        // add 25 W to limit
+                        }
+                        break;
+                    }
+                }
+            }
+            if (!count($limit)) {
                 echo 'nothing to do';
+            } else {
+                if (isset($limit['max'])) {
+                    file_put_contents_print('inverter_power_max', $limit['max']);
+                } 
+                if (isset($limit['min'])) {
+                    file_put_contents_print('inverter_power_min', $limit['min']);
+                }
             }
         } else {
             echo 'timestamp too old';
