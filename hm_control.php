ed97494bba4c1906daef51f07a384faf4348a99f
eenemeenemuu <eenemeenemuu@googlemail.com>
2024-10-13 16:47:35 +0200

    Added button to turn off inverter.

--- hm_control.php
+++ hm_control.php
@@ -59,8 +59,11 @@ if ($hm_control_config_override['override_valid_until'] > time()) {
     $hm_control_config_override['override_valid_until_m'] = floor($countdown / 60);
     $countdown -= $hm_control_config_override['override_valid_until_m']*60;
     $hm_control_config_override['override_valid_until_s'] = $countdown;
+} else {
+    $power_off_button_add_time = "document.getElementById('override_valid_until_h').value=1;";
 }
 
+$power_off_button = '<input type="button" name="power_off" value="Power OFF" onclick="document.getElementById(\'inverter_power_max\').value=-999999;'.$power_off_button_add_time.'document.getElementById(\'save\').click();" />';
 $slider_js = "oninput=document.getElementById('power_target').value=this.value;document.getElementById('power_target_lower_threshold').value=Math.abs(this.value);document.getElementById('power_target_upper_threshold').value=Math.abs(this.value);";
 
 echo '<html><head><title>Hoymiles zero export</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width" /></head><body>';
@@ -68,14 +71,14 @@ if ($message) {
     echo '<div id="message">'.$message.'</div><script type="text/javascript">function hide_message() { document.getElementById(\'message\').style.display = \'none\' } setTimeout(hide_message, 5000)</script>';
 }
 echo '<form method="post"><table border="1"><tr><th onclick="window.location=\'?\'"></th><th>config value</th><th>config override value</th><th>new value</th></tr>';
-echo '<tr><td>override_valid_until</td><td>N/A</td><td>'.date("d.m.Y H:i:s", $hm_control_config_override['override_valid_until']).'</td><td align="right">Days: <input type="text" size="2" name="override_valid_until_d" value="'.$hm_control_config_override['override_valid_until_d'].'" /><br />Hours: <input type="text" size="2" name="override_valid_until_h" value="'.$hm_control_config_override['override_valid_until_h'].'" /><br />Minutes: <input type="text" size="2" name="override_valid_until_m" value="'.$hm_control_config_override['override_valid_until_m'].'" /><br />Seconds: <input type="text" size="2" name="override_valid_until_s" value="'.$hm_control_config_override['override_valid_until_s'].'" /></td></tr>';
-echo '<tr><td>inverter_power_min</td><td>'.$hm_control_config['inverter_power_min'].'</td><td>'.$hm_control_config_override['inverter_power_min'].'</td><td align="right"><input type="text" size="2" name="inverter_power_min" value="'.$hm_control_config_override['inverter_power_min'].'" /></td></tr>';
-echo '<tr><td>inverter_power_max</td><td>'.$hm_control_config['inverter_power_max'].'</td><td>'.$hm_control_config_override['inverter_power_max'].'</td><td align="right"><input type="text" size="2" name="inverter_power_max" value="'.$hm_control_config_override['inverter_power_max'].'" /></td></tr>';
+echo '<tr><td>override_valid_until</td><td>N/A</td><td>'.date("d.m.Y H:i:s", $hm_control_config_override['override_valid_until']).'</td><td align="right">Days: <input type="text" size="2" name="override_valid_until_d" value="'.$hm_control_config_override['override_valid_until_d'].'" /><br />Hours: <input type="text" size="2" name="override_valid_until_h" id="override_valid_until_h" value="'.$hm_control_config_override['override_valid_until_h'].'" /><br />Minutes: <input type="text" size="2" name="override_valid_until_m" value="'.$hm_control_config_override['override_valid_until_m'].'" /><br />Seconds: <input type="text" size="2" name="override_valid_until_s" value="'.$hm_control_config_override['override_valid_until_s'].'" /></td></tr>';
+echo '<tr><td>inverter_power_min</td><td>'.$hm_control_config['inverter_power_min'].'</td><td>'.$hm_control_config_override['inverter_power_min'].'</td><td align="right"><input type="text" size="2" name="inverter_power_min" id="inverter_power_min" value="'.$hm_control_config_override['inverter_power_min'].'" /></td></tr>';
+echo '<tr><td>inverter_power_max</td><td>'.$hm_control_config['inverter_power_max'].'</td><td>'.$hm_control_config_override['inverter_power_max'].'</td><td align="right"><input type="text" size="2" name="inverter_power_max" id="inverter_power_max" value="'.$hm_control_config_override['inverter_power_max'].'" /></td></tr>';
 echo '<tr><td>power_target</td><td>'.$hm_control_config['power_target'].'</td><td>'.$hm_control_config_override['power_target'].'</td><td align="right"><input type="text" size="2" name="power_target" id="power_target" value="'.$hm_control_config_override['power_target'].'" /></td></tr>';
 echo '<tr><td>power_target_lower_threshold</td><td>'.$hm_control_config['power_target_lower_threshold'].'</td><td>'.$hm_control_config_override['power_target_lower_threshold'].'</td><td align="right"><input type="text" size="2" name="power_target_lower_threshold" id="power_target_lower_threshold" value="'.$hm_control_config_override['power_target_lower_threshold'].'" /></td></tr>';
 echo '<tr><td>power_target_upper_threshold</td><td>'.$hm_control_config['power_target_upper_threshold'].'</td><td>'.$hm_control_config_override['power_target_upper_threshold'].'</td><td align="right"><input type="text" size="2" name="power_target_upper_threshold" id="power_target_upper_threshold" value="'.$hm_control_config_override['power_target_upper_threshold'].'" /></td></tr>';
 echo '<tr><td colspan="4" align="right"><input type="range" min="-'.round($hm_control_config['inverter_power_max']/2).'" max="'.round($hm_control_config['inverter_power_max']/2).'" value="'.(isset($hm_control_config_override['power_target']) ? $hm_control_config_override['power_target'] : $hm_control_config['power_target']).'" '.$slider_js.' /></td></tr>';
-echo '<tr><td colspan="3" align="right"><input type="button" name="delete" onclick="window.location=\'?delete\'" value="Delete values" /></td><td align="right"><input type="submit" name="save" value="Save values" /></td></tr>';
+echo '<tr><td colspan="4" align="right">'.$power_off_button.' <input type="button" name="delete" onclick="window.location=\'?delete\'" value="Delete values" /> <input type="submit" name="save" id="save" value="Save values" /></td></tr>';
 echo '</table></form></body></html>';
 
 //EOF
